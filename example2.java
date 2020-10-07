5941be5abe47ac9c92bf7b6ee885bd4bb8ef42cc
Petrushka228 <borovinskiyartem@yandex.ru>
2020-10-07 13:57:45 +0500

    changes

---
+++ example2.java
@@ -0,0 +1,14 @@
+package 1;
+
+class Example
+{
+	public static void main(String[] args)
+	{
+		String s = "Because, sience!";
+		System.out.println("Отладочная линия")
+		
+		System.out.println("In new branch");
+		System.out.println(s);
+		System.out.println("Changes from Nikita");
+	}
+}
---
+++ example3.java
@@ -0,0 +1,13 @@
+package 1;
+
+class Example
+{
+	public static void main(String[] args)
+	{
+		String s = "Because, sience!";
+		System.out.println("Отладочная линия")
+		
+		System.out.println("In new branch");
+		System.out.println(s);
+	}
+}
